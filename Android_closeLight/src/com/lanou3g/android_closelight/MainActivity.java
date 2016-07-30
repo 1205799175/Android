774f9f48e453de1592845b60ae96ebf80a4e4506
杨宇宁 <1205799175@qq.com>
2016-07-30 19:40:45 +0800

    Android

---
+++ Android_closeLight/src/com/lanou3g/android_closelight/MainActivity.java
@@ -0,0 +1,189 @@
+package com.lanou3g.android_closelight;
+
+import java.util.ArrayList;
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity  {
+	//定义存button的arraylist
+	private ArrayList<Button> arrayList;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		arrayList = new ArrayList<Button>();
+		
+		arrayList.add((Button) findViewById(R.id.button_1));
+		arrayList.add((Button) findViewById(R.id.button_2));
+		arrayList.add((Button) findViewById(R.id.button_3));
+		arrayList.add((Button) findViewById(R.id.button_4));
+		arrayList.add((Button) findViewById(R.id.button_5));
+		arrayList.add((Button) findViewById(R.id.button_6));
+		arrayList.add((Button) findViewById(R.id.button_7));
+		arrayList.add((Button) findViewById(R.id.button_8));
+		arrayList.add((Button) findViewById(R.id.button_9));
+		arrayList.add((Button) findViewById(R.id.button_10));
+		arrayList.add((Button) findViewById(R.id.button_11));
+		arrayList.add((Button) findViewById(R.id.button_12));
+		arrayList.add((Button) findViewById(R.id.button_13));
+		arrayList.add((Button) findViewById(R.id.button_14));
+		arrayList.add((Button) findViewById(R.id.button_15));
+		arrayList.add((Button) findViewById(R.id.button_16));
+		arrayList.add((Button) findViewById(R.id.button_17));
+		arrayList.add((Button) findViewById(R.id.button_18));
+		arrayList.add((Button) findViewById(R.id.button_19));
+		arrayList.add((Button) findViewById(R.id.button_20));
+		arrayList.add((Button) findViewById(R.id.button_21));
+		arrayList.add((Button) findViewById(R.id.button_22));
+		arrayList.add((Button) findViewById(R.id.button_23));
+		arrayList.add((Button) findViewById(R.id.button_24));
+		arrayList.add((Button) findViewById(R.id.button_25));
+		arrayList.add((Button) findViewById(R.id.button_26));
+		arrayList.add((Button) findViewById(R.id.button_27));
+		arrayList.add((Button) findViewById(R.id.button_28));
+		arrayList.add((Button) findViewById(R.id.button_29));
+		arrayList.add((Button) findViewById(R.id.button_30));
+		arrayList.add((Button) findViewById(R.id.button_31));
+		arrayList.add((Button) findViewById(R.id.button_32));
+		arrayList.add((Button) findViewById(R.id.button_33));
+		arrayList.add((Button) findViewById(R.id.button_34));
+		arrayList.add((Button) findViewById(R.id.button_35));
+		arrayList.add((Button) findViewById(R.id.button_36));
+		arrayList.add((Button) findViewById(R.id.button_37));
+		arrayList.add((Button) findViewById(R.id.button_38));
+		arrayList.add((Button) findViewById(R.id.button_39));
+		arrayList.add((Button) findViewById(R.id.button_40));
+		arrayList.add((Button) findViewById(R.id.button_41));
+		arrayList.add((Button) findViewById(R.id.button_42));
+		arrayList.add((Button) findViewById(R.id.button_43));
+		arrayList.add((Button) findViewById(R.id.button_44));
+		arrayList.add((Button) findViewById(R.id.button_45));
+		arrayList.add((Button) findViewById(R.id.button_46));
+		arrayList.add((Button) findViewById(R.id.button_47));
+		arrayList.add((Button) findViewById(R.id.button_48));
+		arrayList.add((Button) findViewById(R.id.button_49));
+		arrayList.add((Button) findViewById(R.id.button_50));
+		arrayList.add((Button) findViewById(R.id.button_51));
+		arrayList.add((Button) findViewById(R.id.button_52));
+		arrayList.add((Button) findViewById(R.id.button_53));
+		arrayList.add((Button) findViewById(R.id.button_54));
+		arrayList.add((Button) findViewById(R.id.button_55));
+		arrayList.add((Button) findViewById(R.id.button_56));
+		arrayList.add((Button) findViewById(R.id.button_57));
+		arrayList.add((Button) findViewById(R.id.button_58));
+		arrayList.add((Button) findViewById(R.id.button_59));
+		arrayList.add((Button) findViewById(R.id.button_60));
+		arrayList.add((Button) findViewById(R.id.button_61));
+		arrayList.add((Button) findViewById(R.id.button_62));
+		arrayList.add((Button) findViewById(R.id.button_63));
+		arrayList.add((Button) findViewById(R.id.button_64));
+		arrayList.add((Button) findViewById(R.id.button_65));
+		arrayList.add((Button) findViewById(R.id.button_66));
+		arrayList.add((Button) findViewById(R.id.button_67));
+		arrayList.add((Button) findViewById(R.id.button_68));
+		arrayList.add((Button) findViewById(R.id.button_69));
+		arrayList.add((Button) findViewById(R.id.button_70));
+		arrayList.add((Button) findViewById(R.id.button_71));
+		arrayList.add((Button) findViewById(R.id.button_72));
+		arrayList.add((Button) findViewById(R.id.button_73));
+		arrayList.add((Button) findViewById(R.id.button_74));
+		arrayList.add((Button) findViewById(R.id.button_75));
+		arrayList.add((Button) findViewById(R.id.button_76));
+		arrayList.add((Button) findViewById(R.id.button_77));
+		arrayList.add((Button) findViewById(R.id.button_78));
+		arrayList.add((Button) findViewById(R.id.button_79));
+		arrayList.add((Button) findViewById(R.id.button_80));
+		arrayList.add((Button) findViewById(R.id.button_81));
+		arrayList.add((Button) findViewById(R.id.button_82));
+		arrayList.add((Button) findViewById(R.id.button_83));
+		arrayList.add((Button) findViewById(R.id.button_84));
+		arrayList.add((Button) findViewById(R.id.button_85));
+		arrayList.add((Button) findViewById(R.id.button_86));
+		arrayList.add((Button) findViewById(R.id.button_87));
+		arrayList.add((Button) findViewById(R.id.button_88));
+		arrayList.add((Button) findViewById(R.id.button_89));
+		arrayList.add((Button) findViewById(R.id.button_90));
+		arrayList.add((Button) findViewById(R.id.button_91));
+		arrayList.add((Button) findViewById(R.id.button_92));
+		arrayList.add((Button) findViewById(R.id.button_93));
+		arrayList.add((Button) findViewById(R.id.button_94));
+		arrayList.add((Button) findViewById(R.id.button_95));
+		arrayList.add((Button) findViewById(R.id.button_96));
+		arrayList.add((Button) findViewById(R.id.button_97));
+		arrayList.add((Button) findViewById(R.id.button_98));
+		arrayList.add((Button) findViewById(R.id.button_99));
+		arrayList.add((Button) findViewById(R.id.button_100));
+		arrayList.add((Button) findViewById(R.id.button_101));
+		arrayList.add((Button) findViewById(R.id.button_102));
+		arrayList.add((Button) findViewById(R.id.button_103));
+		arrayList.add((Button) findViewById(R.id.button_104));
+		arrayList.add((Button) findViewById(R.id.button_105));
+		arrayList.add((Button) findViewById(R.id.button_106));
+		arrayList.add((Button) findViewById(R.id.button_107));
+		arrayList.add((Button) findViewById(R.id.button_108));
+	}
+	
+	public void buttonClick(View v) {
+        //获取当前背景的颜色
+		 ColorDrawable colorDrawable = (ColorDrawable) v.getBackground();
+	        if (colorDrawable.getColor()== Color.parseColor("#99cc00")) {
+	            v.setBackgroundColor(Color.RED);
+	        }else{
+	                v.setBackgroundColor(Color.parseColor("#99cc00"));
+	        }
+	        
+	        int index = Integer.parseInt((String) v.getTag());
+	        
+	        //上
+	        if (index > 9) {
+	            Button upBtu = arrayList.get(index - 10);
+	            ColorDrawable upColorDrawable = (ColorDrawable) upBtu.getBackground();
+	            //获取当前背景的颜色
+	            if (upColorDrawable.getColor() == Color.parseColor("#99cc00")) {
+	                upBtu.setBackgroundColor(Color.RED);
+	            } else {
+	                upBtu.setBackgroundColor(Color.parseColor("#99cc00"));
+	            }
+	        }
+	        //下
+	        if (index < 100) {
+	            Button upBtu = arrayList.get(index + 8);
+	            ColorDrawable upColorDrawable = (ColorDrawable) upBtu.getBackground();
+	            //获取当前背景的颜色
+	            if (upColorDrawable.getColor() == Color.parseColor("#99cc00")) {
+	                upBtu.setBackgroundColor(Color.RED);
+	            } else {
+	                upBtu.setBackgroundColor(Color.parseColor("#99cc00"));
+	            }
+	        }
+	        //右
+	        if (index % 9 !=0) {
+	            Button upBtu = arrayList.get(index);
+	            ColorDrawable upColorDrawable = (ColorDrawable) upBtu.getBackground();
+	            //获取当前背景的颜色
+	            if (upColorDrawable.getColor() == Color.parseColor("#99cc00")) {
+	                upBtu.setBackgroundColor(Color.RED);
+	            } else {
+	                upBtu.setBackgroundColor(Color.parseColor("#99cc00"));
+	            }
+	        }
+	        //左
+	        if (index % 9 != 1) {
+	            Button upBtu = arrayList.get(index - 2);
+	            ColorDrawable upColorDrawable = (ColorDrawable) upBtu.getBackground();
+	            //获取当前背景的颜色
+	            if (upColorDrawable.getColor() == Color.parseColor("#99cc00")) {
+	                upBtu.setBackgroundColor(Color.RED);
+	            } else {
+	                upBtu.setBackgroundColor(Color.parseColor("#99cc00"));
+	            }
+	        }
+	}
+
+}
